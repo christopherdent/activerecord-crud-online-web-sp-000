f15fa18ce96cd9e2f3fc303f331f8522c3ce937f
Christopher Dent <christopherdent01@gmail.com>
2019-11-09 01:29:25 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(arguments: )
   	Movie.create do |m|
      Movie.new("Home Alone")
   end	  
