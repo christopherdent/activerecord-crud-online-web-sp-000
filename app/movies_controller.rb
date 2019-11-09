2921e0a63981713bb9e9ced97d3e39f6c61fdcf4
Christopher Dent <christopherdent01@gmail.com>
2019-11-09 01:52:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -50,8 +50,9 @@ def can_find_the_first_item_from_the_database_using_id
   Movie.find(1)
 end
 
-def can_find_by_multiple_attributes
+def can_find_by_multiple_attributes(args = {title: 'Title', release_date: 2000, director: 'Me'})
   
+  Movie.find_by(args)
   
   # Search Values:
   # title == "Title"
