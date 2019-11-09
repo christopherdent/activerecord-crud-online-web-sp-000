73ab157dbb9b4ad1b4405391750990bedc608cb8
Christopher Dent <christopherdent01@gmail.com>
2019-11-09 01:53:23 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -58,6 +58,8 @@ def can_find_by_multiple_attributes(args = {title: 'Title', release_date: 2000, 
 end
 
 def can_find_using_where_clause_and_be_sorted
+  
+  
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
   __
