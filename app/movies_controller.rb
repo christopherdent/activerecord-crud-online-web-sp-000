40048f92fb48fcbbeeb3aafa93ffe326be3881e7
Christopher Dent <christopherdent01@gmail.com>
2019-11-09 01:26:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,9 +24,9 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block
-  	  Movie.create do |m|
-     Movie.new
+def can_be_created_in_a_block(arg)
+  	Movie.create do |m|
+     Movie.new(arg)
   end	  
 end	
 
